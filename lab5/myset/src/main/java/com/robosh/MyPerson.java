733eff09ef7c1b2696931affc57318dc80a310ae
Erik Arutiunian2 <pturnavanguard@gmail.com>
2019-12-09 13:17:07 +0200

    Added another function to MyPerson in lab5

--- lab5/myset/src/main/java/com/robosh/MyPerson.java
+++ lab5/myset/src/main/java/com/robosh/MyPerson.java
@@ -20,6 +20,10 @@ public class MyPerson {
         return ("Name: " + name + " age: " + age);
     }
 
+    public void sayNo(){
+        System.out.println("No");
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
